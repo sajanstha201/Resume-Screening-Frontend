c8a2c248297a88ff4e5326572bdc764d4641d5df
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-23 09:37:23 +0545

    done with the layout of the uploaded resume

--- src/components/ResumeScreening/UploadedResume.js
+++ src/components/ResumeScreening/UploadedResume.js
@@ -3,6 +3,11 @@ export const UploadedResume=({resumeDetail,setResumeDetail})=>{
         <div className='h-[60%] flex flex-col shadow-md items-center justify-center '>
                 <h2>This is uploaded resume section</h2>
                 <div className="h-[70%] w-[80%] flex-wrap overflow-auto border border-2 border-green-400 rounded-md">
+                    <div className="border border-black w-[80px] h-[100px] rounded-md m-2 relative bg-green-400 overflow-hidden p-2">
+                    <div className="bg-red-600 w-[15px] h-[15px] rounded-full right-1 top-1 absolute flex items-center justify-center pb-1 border border-black border-1 hover:w-[17px] hover:h-[17px]">x</div>
+
+                    </div>
+
                 </div>
                 <div className="flex gap-2 m-2">
                     <button  className="w-28 h-10 bg-red-500 rounded-md border-2 border border-black shadow-sm">Remove All</button>
